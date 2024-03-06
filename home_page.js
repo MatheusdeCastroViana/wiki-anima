b35cf77751df65b6c4001ecbcc7d117196988ec8
Pedro Henrique <pedro.prado.alves239@gmail.com>
2024-03-06 20:10:19 -0300

    Criação do carrossel de personagens

--- home_page.js
+++ home_page.js
@@ -2,6 +2,15 @@ $('.carousel-destaques').slick({
   dots: true,
   infinite: true,
   slidesToShow: 6,
-  slidesToScroll: 6,
+  slidesToScroll: 3,
+  variableWidth: true
+});
+
+
+$('.carousel-personagens').slick({
+  dots: true,
+  infinite: true,
+  slidesToShow: 6,
+  slidesToScroll: 3,
   variableWidth: true
 });
