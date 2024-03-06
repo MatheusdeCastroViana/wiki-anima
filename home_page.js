f19818647e5da26e488d6a3ba55af94aab3659d3
Pedro Henrique <pedro.prado.alves239@gmail.com>
2024-03-05 22:20:56 -0300

    update
    Carrossel funcionando com o Slick Slider

--- home_page.js
+++ home_page.js
@@ -1,13 +1,5 @@
-let currentIndex = 0;
-const carouselItems = document.querySelectorAll('.carousel-item');
-
-function moveCarousel(direction) {
-  currentIndex += direction;
-  if (currentIndex < 0) {
-    currentIndex = carouselItems.length - 1;
-  } else if (currentIndex >= carouselItems.length) {
-    currentIndex = 0;
-  }
-  const offset = -currentIndex * (document.querySelector('.carousel-item').offsetWidth + 20) + 'px'; /* Largura da imagem + margem */
-  document.querySelector('.carousel').style.transform = `translateX(${offset})`;
-}
+$('.carousel-destaques').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3
+});
